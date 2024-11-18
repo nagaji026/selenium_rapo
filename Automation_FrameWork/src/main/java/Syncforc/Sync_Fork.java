d3854d22f0768349b94d41ce9032196781cada4e
ADMIN <ADMIN@Naveen>
2024-11-18 12:50:11 +0530

    syncfork class added

---
+++ Automation_FrameWork/src/main/java/Syncforc/Sync_Fork.java
@@ -0,0 +1,12 @@
+package Syncforc;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sync_Fork {
+	@Test
+	public void sync() {
+		Reporter.log("syncfork class added");
+	}
+
+}
